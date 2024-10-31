cc9a9c1843850055b7c0eb5d9507770b1c66fe7c
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-10-31 17:14:06 +0100

    fc

---
+++ src/main/java/net/axel/ITLens/ItLensApplication.java
@@ -0,0 +1,13 @@
+package net.axel.ITLens;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItLensApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItLensApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/axel/ITLens/ItLensApplicationTests.java
@@ -0,0 +1,13 @@
+package net.axel.ITLens;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItLensApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
