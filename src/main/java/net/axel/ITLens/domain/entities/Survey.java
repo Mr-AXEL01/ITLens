c6cfd4825e33024aa1e6eee44db130f8ec8e35ba
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-11-01 02:43:43 +0100

    Feat : add entity
    add the Servey entity and set the getter and setter and default constructor and all args constructor with lombock annotations + implement the relationship with Owner.

---
+++ src/main/java/net/axel/ITLens/domain/entities/Survey.java
@@ -0,0 +1,36 @@
+package net.axel.ITLens.domain.entities;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+import lombok.experimental.Accessors;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+@Entity
+@Table(name = "surveys")
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Accessors(chain = true)
+public class Survey implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @NotBlank
+    @Column(name = "survey_title")
+    private String title;
+
+    @NotBlank
+    @Column(name = "survey_descreption")
+    private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id", nullable = false)
+    private Owner owner;
+}
