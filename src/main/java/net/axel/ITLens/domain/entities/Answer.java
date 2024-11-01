196a5d772ac2e1e74129bd9cac6f4478d9933b37
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-11-01 12:25:02 +0100

    Feat : add entity
    set entity for the answer and set the loiberplate code with lombok 'getter && setter && constructor' + implement the relationship with Question.

---
+++ src/main/java/net/axel/ITLens/domain/entities/Answer.java
@@ -0,0 +1,36 @@
+package net.axel.ITLens.domain.entities;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+import lombok.experimental.Accessors;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+@Entity
+@Table(name = "answers")
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Accessors(chain = true)
+public class Answer implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @NotBlank
+    @Column(name = "text")
+    private String text;
+
+    @Column(name = "selection_count")
+    private Integer selectionCount;
+
+    @ManyToOne
+    @JoinColumn(name = "question_id", nullable = false)
+    private Question question;
+
+}
