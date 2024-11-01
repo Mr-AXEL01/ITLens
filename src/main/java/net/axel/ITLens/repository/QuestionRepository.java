bce96e0ffedd6cb6e28b59752cf9c900448bb162
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-11-01 14:36:59 +0100

    Feat : add repository
    set repository for the Question and extends from the JpaRepository to let Spring Data handle the DB side

---
+++ src/main/java/net/axel/ITLens/repository/QuestionRepository.java
@@ -0,0 +1,9 @@
+package net.axel.ITLens.repository;
+
+import net.axel.ITLens.domain.entities.Question;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface QuestionRepository extends JpaRepository<Question, UUID> {
+}
