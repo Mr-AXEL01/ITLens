0d5436f205623abc457ec3a8e1b568d63be30554
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-11-05 20:15:36 +0100

    Feat : override the updateEntity method
    override the updateEntity method to set new vlue for the updated attributes.

--- src/main/java/net/axel/ITLens/service/implementations/SurveyService.java
+++ src/main/java/net/axel/ITLens/service/implementations/SurveyService.java
@@ -1,8 +1,12 @@
 package net.axel.ITLens.service.implementations;
 
+import jakarta.transaction.Transactional;
+import net.axel.ITLens.domain.dtos.owner.OwnerResponseDTO;
 import net.axel.ITLens.domain.dtos.survey.SurveyRequestDTO;
 import net.axel.ITLens.domain.dtos.survey.SurveyResponseDTO;
+import net.axel.ITLens.domain.entities.Owner;
 import net.axel.ITLens.domain.entities.Survey;
+import net.axel.ITLens.mapper.OwnerMapper;
 import net.axel.ITLens.mapper.SurveyMapper;
 import net.axel.ITLens.repository.SurveyRepository;
 import net.axel.ITLens.service.interfaces.ISurveyService;
@@ -11,14 +15,25 @@ import org.springframework.stereotype.Service;
 import java.util.UUID;
 
 @Service
+@Transactional
 public class SurveyService extends BaseService<Survey, SurveyRequestDTO, SurveyResponseDTO, UUID> implements ISurveyService {
 
-    public SurveyService(SurveyRepository repository, SurveyMapper mapper) {
+    private final OwnerService ownerService;
+    private final OwnerMapper ownerMapper;
+
+    public SurveyService(SurveyRepository repository, SurveyMapper mapper, OwnerService ownerService, OwnerMapper ownerMapper) {
         super(repository, mapper);
+        this.ownerService = ownerService;
+        this.ownerMapper = ownerMapper;
     }
 
     @Override
     protected void updateEntity(Survey survey, SurveyRequestDTO dto) {
+        OwnerResponseDTO ownerResponse = ownerService.getById(dto.ownerId());
+        Owner owner = ownerMapper.toEntityFromResponseDto(ownerResponse);
 
+        survey.setTitle(dto.title())
+                .setDescription(dto.description())
+                .setOwner(owner);
     }
 }
