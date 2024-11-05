9318822bda437ad93822b1926d51027dcb8555be
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-11-05 20:13:56 +0100

    Feat : extends from BaseController
    extends from the baseController to get all the public and protected methods.

--- src/main/java/net/axel/ITLens/controller/SurveyController.java
+++ src/main/java/net/axel/ITLens/controller/SurveyController.java
@@ -1,14 +1,20 @@
 package net.axel.ITLens.controller;
 
+import net.axel.ITLens.domain.dtos.survey.SurveyRequestDTO;
+import net.axel.ITLens.domain.dtos.survey.SurveyResponseDTO;
+import net.axel.ITLens.domain.entities.Survey;
+import net.axel.ITLens.service.interfaces.ISurveyService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.UUID;
+
 @RestController
 @RequestMapping(SurveyController.CONTROLLER_PATH)
-public class SurveyController {
+public class SurveyController extends BaseController<Survey, SurveyRequestDTO, SurveyResponseDTO, UUID> {
     public final static String CONTROLLER_PATH = "/api/v1/surveys";
 
-    public SurveyController(ISurvey) {
-
+    public SurveyController(ISurveyService baseService) {
+        super(baseService);
     }
 }
