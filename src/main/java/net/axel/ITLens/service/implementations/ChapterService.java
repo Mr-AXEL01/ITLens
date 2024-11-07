c6c9aee73cac54f71d269cf35fab135a4e240f47
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-11-07 20:45:05 +0100

    Feat : inject beans
    inject the surveyEdition mapper and service in the chapterService.

--- src/main/java/net/axel/ITLens/service/implementations/ChapterService.java
+++ src/main/java/net/axel/ITLens/service/implementations/ChapterService.java
@@ -4,9 +4,12 @@ import jakarta.transaction.Transactional;
 import net.axel.ITLens.domain.dtos.chapter.ChapterRequestDTO;
 import net.axel.ITLens.domain.dtos.chapter.ChapterResponseDTO;
 import net.axel.ITLens.domain.entities.Chapter;
+import net.axel.ITLens.domain.entities.SurveyEdition;
 import net.axel.ITLens.mapper.ChapterMapper;
+import net.axel.ITLens.mapper.SurveyEditionMapper;
 import net.axel.ITLens.repository.ChapterRepository;
 import net.axel.ITLens.service.interfaces.IChapterService;
+import net.axel.ITLens.service.interfaces.ISurveyEditionService;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
@@ -15,12 +18,27 @@ import java.util.UUID;
 @Transactional
 public class ChapterService extends BaseService<Chapter, ChapterRequestDTO, ChapterResponseDTO, UUID> implements IChapterService {
 
-    public ChapterService(ChapterRepository repository, ChapterMapper mapper) {
+    private final ISurveyEditionService surveyEditionService;
+
+    private final SurveyEditionMapper surveyEditionMapper;
+
+    public ChapterService(ChapterRepository repository, ChapterMapper mapper, ISurveyEditionService surveyEditionService, SurveyEditionMapper surveyEditionMapper) {
         super(repository, mapper);
+        this.surveyEditionService = surveyEditionService;
+        this.surveyEditionMapper = surveyEditionMapper;
+    }
+
+    @Override
+    public ChapterResponseDTO create(ChapterRequestDTO dto) {
+
     }
 
     @Override
     protected void updateEntity(Chapter chapter, ChapterRequestDTO dto) {
+
+    }
+
+    private SurveyEdition surveyEdition(UUID surveyEditionId) {
 
     }
 }
