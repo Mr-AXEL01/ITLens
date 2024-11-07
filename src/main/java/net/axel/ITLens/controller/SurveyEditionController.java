73146599b017706ebc41f4dde286a1b1051a2097
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-11-07 19:27:05 +0100

    Feat : add controller
    set controller for the SurveyEdition and extends from the baseController
    to avoid the boilerplate code.

--- src/main/java/net/axel/ITLens/controller/SurveyEditionController.java
+++ src/main/java/net/axel/ITLens/controller/SurveyEditionController.java
@@ -1,14 +1,20 @@
 package net.axel.ITLens.controller;
 
+import net.axel.ITLens.domain.dtos.surveyEdition.SurveyEditionRequestDTO;
+import net.axel.ITLens.domain.dtos.surveyEdition.SurveyEditionResponseDTO;
+import net.axel.ITLens.domain.entities.SurveyEdition;
+import net.axel.ITLens.service.interfaces.ISurveyEditionService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.UUID;
+
 @RestController
 @RequestMapping(SurveyEditionController.CONTROLLER_PATH)
-public class SurveyEditionController {
+public class SurveyEditionController extends BaseController<SurveyEdition, SurveyEditionRequestDTO, SurveyEditionResponseDTO, UUID> {
     public final static String CONTROLLER_PATH = "/api/v1/survey_editions";
 
-    public SurveyEditionController() {
-        super();
+    public SurveyEditionController(ISurveyEditionService baseService) {
+        super(baseService);
     }
 }
