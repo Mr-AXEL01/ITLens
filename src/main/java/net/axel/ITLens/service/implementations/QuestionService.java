25f1844298ee5169abf48ba500912596a424bfb1
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-11-08 22:47:32 +0100

    Feat : add a private method
    add a privte method to map the answers request to an entity and set them in case the create or the update methods.

--- src/main/java/net/axel/ITLens/service/implementations/QuestionService.java
+++ src/main/java/net/axel/ITLens/service/implementations/QuestionService.java
@@ -1,11 +1,14 @@
 package net.axel.ITLens.service.implementations;
 
 import jakarta.transaction.Transactional;
+import net.axel.ITLens.domain.dtos.answer.AnswerRequestDTO;
 import net.axel.ITLens.domain.dtos.chapter.ChapterResponseDTO;
 import net.axel.ITLens.domain.dtos.question.QuestionRequestDTO;
 import net.axel.ITLens.domain.dtos.question.QuestionResponseDTO;
+import net.axel.ITLens.domain.entities.Answer;
 import net.axel.ITLens.domain.entities.Chapter;
 import net.axel.ITLens.domain.entities.Question;
+import net.axel.ITLens.mapper.AnswerMapper;
 import net.axel.ITLens.mapper.ChapterMapper;
 import net.axel.ITLens.mapper.QuestionMapper;
 import net.axel.ITLens.repository.QuestionRepository;
@@ -13,26 +16,31 @@ import net.axel.ITLens.service.interfaces.IChapterService;
 import net.axel.ITLens.service.interfaces.IQuestionService;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Set;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional
 public class QuestionService extends BaseService<Question, QuestionRequestDTO, QuestionResponseDTO, UUID> implements IQuestionService {
 
     private final IChapterService chapterService;
-
     private final ChapterMapper chapterMapper;
+    private final AnswerMapper answerMapper;
 
-    public QuestionService(QuestionRepository repository, QuestionMapper mapper, IChapterService chapterService, ChapterMapper chapterMapper) {
+    public QuestionService(QuestionRepository repository, QuestionMapper mapper, IChapterService chapterService, ChapterMapper chapterMapper, AnswerMapper answerMapper) {
         super(repository, mapper);
         this.chapterService = chapterService;
         this.chapterMapper = chapterMapper;
+        this.answerMapper = answerMapper;
     }
 
     @Override
     public QuestionResponseDTO create(QuestionRequestDTO dto) {
         Question question = mapper.toEntity(dto)
-                .setChapter(chapter(dto.chapterId()));
+                .setChapter(chapter(dto.chapterId()))
+                .setAnswers(mapAnswersRequestToEntity(dto.answers()));
 
         Question savedChapter = repository.save(question);
 
@@ -43,11 +51,18 @@ public class QuestionService extends BaseService<Question, QuestionRequestDTO, Q
     protected void updateEntity(Question question, QuestionRequestDTO dto) {
         question.setQuestionType(dto.questionType())
                 .setText(dto.text())
-                .setChapter(chapter(dto.chapterId()));
+                .setChapter(chapter(dto.chapterId()))
+                .setAnswers(mapAnswersRequestToEntity(dto.answers()));
     }
 
     private Chapter chapter(UUID chapterId) {
         ChapterResponseDTO chapterResponse = chapterService.getById(chapterId);
         return chapterMapper.toEntityFromResponseDto(chapterResponse);
+    }
+
+    private Set<Answer> mapAnswersRequestToEntity(List<AnswerRequestDTO> answersRequest) {
+        return answersRequest.stream()
+                .map(answerMapper::toEntity)
+                .collect(Collectors.toSet());
     }
 }
