e012410144a1bc1503842099412e44295d9a2337
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-11-08 22:42:36 +0100

    Feat : add attribute
    add list of answers to the question request

--- src/main/java/net/axel/ITLens/domain/dtos/question/QuestionRequestDTO.java
+++ src/main/java/net/axel/ITLens/domain/dtos/question/QuestionRequestDTO.java
@@ -2,8 +2,10 @@ package net.axel.ITLens.domain.dtos.question;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import net.axel.ITLens.domain.dtos.answer.AnswerRequestDTO;
 import net.axel.ITLens.domain.enums.QuestionType;
 
+import java.util.List;
 import java.util.UUID;
 
 public record QuestionRequestDTO(
@@ -11,6 +13,8 @@ public record QuestionRequestDTO(
 
         @NotBlank QuestionType questionType,
 
-        @NotNull UUID chapterId
+        @NotNull UUID chapterId,
+
+        @NotNull List<AnswerRequestDTO> answers
 ) {
 }
