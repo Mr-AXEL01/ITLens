e044a78595978e2b3e6b66de87f7b7fa932f4f17
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-11-08 20:53:43 +0100

    Feat : set a private method to get the chapter
    inject the chapter service and mapper to use in a private method to get the chapter

---
+++ src/main/java/net/axel/ITLens/service/implementations/QuestionService.java
@@ -0,0 +1,43 @@
+package net.axel.ITLens.service.implementations;
+
+import jakarta.transaction.Transactional;
+import net.axel.ITLens.domain.dtos.chapter.ChapterResponseDTO;
+import net.axel.ITLens.domain.dtos.question.QuestionRequestDTO;
+import net.axel.ITLens.domain.dtos.question.QuestionResponseDTO;
+import net.axel.ITLens.domain.entities.Chapter;
+import net.axel.ITLens.domain.entities.Question;
+import net.axel.ITLens.mapper.ChapterMapper;
+import net.axel.ITLens.mapper.QuestionMapper;
+import net.axel.ITLens.repository.QuestionRepository;
+import net.axel.ITLens.service.interfaces.IChapterService;
+import net.axel.ITLens.service.interfaces.IQuestionService;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+@Transactional
+public class QuestionService extends BaseService<Question, QuestionRequestDTO, QuestionResponseDTO, UUID> implements IQuestionService {
+
+    private final IChapterService chapterService;
+
+    private final ChapterMapper chapterMapper;
+
+    public QuestionService(QuestionRepository repository, QuestionMapper mapper, IChapterService chapterService, ChapterMapper chapterMapper) {
+        super(repository, mapper);
+        this.chapterService = chapterService;
+        this.chapterMapper = chapterMapper;
+    }
+
+    @Override
+    protected void updateEntity(Question question, QuestionRequestDTO dto) {
+        question.setQuestionType(dto.questionType())
+                .setText(dto.text())
+                .setChapter(chapter(dto.chapterId()));
+    }
+
+    private Chapter chapter(UUID chapterId) {
+        ChapterResponseDTO chapterResponse = chapterService.getById(chapterId);
+        return chapterMapper.toEntityFromResponseDto(chapterResponse);
+    }
+}
