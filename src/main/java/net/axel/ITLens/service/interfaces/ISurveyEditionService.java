965a5527504979ea9983538c6c7512f601b54567
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-11-11 02:58:19 +0100

    Feat : declare new methods.

--- src/main/java/net/axel/ITLens/service/interfaces/ISurveyEditionService.java
+++ src/main/java/net/axel/ITLens/service/interfaces/ISurveyEditionService.java
@@ -1,5 +1,6 @@
 package net.axel.ITLens.service.interfaces;
 
+import net.axel.ITLens.domain.dtos.surveyEdition.ResultsDTO;
 import net.axel.ITLens.domain.dtos.surveyEdition.SurveyEditionRequestDTO;
 import net.axel.ITLens.domain.dtos.surveyEdition.SurveyEditionResponseDTO;
 import net.axel.ITLens.domain.entities.SurveyEdition;
@@ -7,4 +8,5 @@ import net.axel.ITLens.domain.entities.SurveyEdition;
 import java.util.UUID;
 
 public interface ISurveyEditionService extends IBaseService<SurveyEdition, SurveyEditionRequestDTO, SurveyEditionResponseDTO, UUID> {
+    ResultsDTO results(UUID surveyEditionId);
 }
--- src/main/java/net/axel/ITLens/service/interfaces/ISurveyService.java
+++ src/main/java/net/axel/ITLens/service/interfaces/ISurveyService.java
@@ -1,10 +1,13 @@
 package net.axel.ITLens.service.interfaces;
 
+import net.axel.ITLens.domain.dtos.survey.SurveyParticipateResponseDTO;
 import net.axel.ITLens.domain.dtos.survey.SurveyRequestDTO;
 import net.axel.ITLens.domain.dtos.survey.SurveyResponseDTO;
 import net.axel.ITLens.domain.entities.Survey;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface ISurveyService extends IBaseService<Survey, SurveyRequestDTO, SurveyResponseDTO, UUID> {
+    void participate(UUID surveyId, List<SurveyParticipateResponseDTO> responses);
 }
