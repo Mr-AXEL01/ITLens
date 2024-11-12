c3037653d74794b51a6b7dc33808d3dbdd126bc3
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-11-13 00:55:45 +0100

    Feat : add annotation
    set the exist annotation.

---
+++ src/main/java/net/axel/ITLens/validation/Exists.java
@@ -0,0 +1,24 @@
+package net.axel.ITLens.validation;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+import net.axel.ITLens.validation.validator.ExistsValidator;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+@Constraint(validatedBy = ExistsValidator.class)
+public @interface Exists {
+
+    String message() default "Entity does not exist";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+
+    Class<?> entityClass();
+}
