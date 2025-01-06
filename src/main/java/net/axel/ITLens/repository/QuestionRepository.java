ba0dbd32602b0d23a706be1dadaf10549525f3ac
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2025-01-06 11:34:45 +0100

    add new method
    set new method to retrieve the questions by the chapter ID.

--- src/main/java/net/axel/ITLens/repository/QuestionRepository.java
+++ src/main/java/net/axel/ITLens/repository/QuestionRepository.java
@@ -3,7 +3,9 @@ package net.axel.ITLens.repository;
 import net.axel.ITLens.domain.entities.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface QuestionRepository extends JpaRepository<Question, UUID> {
+    List<Question> findQuestionsByChapterId(UUID chapterId);
 }
